d4e03b38514faaeb32e8cc8e8b80122d5cf3e096
ewien <ewin.sutriandi@gmail.com>
2017-02-01 11:25:53 +0800

    add post method for telemetry data receiver

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,11 +4,15 @@ import (
 	"net/http"
 	"time"
 
+	"strconv"
+
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/ewinsutriandi/iotlogger"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
+
+const secretKey string = "ular lari lurus"
 
 func login(c echo.Context) error {
 	username := c.FormValue("username")
@@ -24,7 +28,7 @@ func login(c echo.Context) error {
 		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
 
 		// Generate encoded token and send it as response.
-		t, err := token.SignedString([]byte("secret"))
+		t, err := token.SignedString([]byte(secretKey))
 		if err != nil {
 			return err
 		}
@@ -53,6 +57,7 @@ func main() {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	mdjwt := middleware.JWT([]byte(secretKey))
 
 	// Login route
 	e.POST("/login", login)
@@ -60,10 +65,49 @@ func main() {
 	// Unauthenticated route
 	e.GET("/", accessible)
 
-	// Restricted group
-	r := e.Group("/restricted")
-	r.Use(middleware.JWT([]byte("secret")))
-	r.GET("", restricted)
+	//restricted route group for authenticated user
+	ru := e.Group("/restricted")
+	ru.Use(mdjwt)
+	ru.GET("/user/:usr", getUser)
+	ru.POST("/telem", putTelemReport)
 
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) (err error) {
+	username := c.Param("usr")
+	ctl, err := iotlogger.NewController()
+	user, err := ctl.GetUser(username)
+	return c.JSON(http.StatusOK, user)
+}
+
+func putUser(c echo.Context) (err error) {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+	ctl, err := iotlogger.NewController()
+	if err != nil {
+		return err
+	}
+	err = ctl.AddUser(username, password)
+	return c.JSON(http.StatusOK, "user: "+username+" created")
+}
+
+func putTelemReport(c echo.Context) (err error) {
+	ip := c.RealIP()
+	devname := c.FormValue("devname")
+	metric := c.FormValue("metric")
+	unit := c.FormValue("unit")
+	value, err := strconv.ParseFloat(c.FormValue("value"), 64)
+	if err != nil {
+		iotlogger.HandleError(err)
+		return
+	}
+	telem := iotlogger.NewTelemetryReport(ip, devname, metric, unit, value)
+	ctl, err := iotlogger.NewController()
+	if err != nil {
+		iotlogger.HandleError(err)
+	}
+	err = ctl.AddTelemetryReport(*telem)
+	iotlogger.HandleError(err)
+	return c.JSON(http.StatusCreated, telem)
 }
